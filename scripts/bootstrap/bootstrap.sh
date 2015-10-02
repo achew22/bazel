544d0b0a5024e8920c32aa738ff0b75eb7510c7c
Andrew Z Allen <me@andrewzallen.com>
2015-10-02 00:07:38 -0600

    When bootstrapping Bazel build in batch mode.
    It currently is impossible to build Bazel using ./compile.sh in a chroot
    or Docker image.  That's unfortunate cause it would make bug
    documentation and reproduction much easier. This patch switches the
    bootstrap.sh script to build using "batch" mode. Batch mode "provides
    more predictable semantics with respect to signal handling, job control,
    and environment variable inheritance, and is necessary for running Bazel
    in a chroot jail".
    
    Interestingly, this doesn't actually fix the problem (still working on
    that), but it does make it fail instead of looping forever trying to
    terminate an old Bazel server. Fortunately it fails after it has
    produced a Bazel binary which at least lets me choose to ignore it and
    move on.
    
    Bug: https://github.com/bazelbuild/bazel/issues/134
    Change-Id: Ia008af973f9e63836826dfe65f10e5fbf4967bc6

--- scripts/bootstrap/bootstrap.sh
+++ scripts/bootstrap/bootstrap.sh
@@ -40,12 +40,12 @@ function bazel_bootstrap() {
   if [[ ! ${BAZEL_SKIP_TOOL_COMPILATION-} =~ "$2" ]]; then
     log "Building $2"
     if [ -n "${4-}" ]; then
-      ${BAZEL} --nomaster_bazelrc --bazelrc=${BAZELRC} \
+      ${BAZEL} --nomaster_bazelrc --bazelrc=${BAZELRC} --batch \
           build ${BAZEL_ARGS} \
           --javacopt="-source ${JAVA_VERSION} -target ${JAVA_VERSION}" \
           "${EMBED_LABEL_ARG[@]}" $1
     else
-      run_silent ${BAZEL} --nomaster_bazelrc --bazelrc=${BAZELRC} \
+      run_silent ${BAZEL} --nomaster_bazelrc --bazelrc=${BAZELRC} --batch \
           build ${BAZEL_ARGS} \
           --javacopt="-source ${JAVA_VERSION} -target ${JAVA_VERSION}" \
           "${EMBED_LABEL_ARG[@]}" $1
